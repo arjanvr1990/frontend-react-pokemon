f9d4382912d8bda80f8a39a7fd33b3788ad70e6f
arjanvr1990 <arjanvanraamsdonk@gmail.com>
2023-10-17 18:18:18 +0200

    loading en error toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import { useEffect, useState } from 'react';
 import pokemonImage from "./assets/Pokemon.png"
 
 function App() {
-    const [pokemonList, setPokemonList] = useState([]); // Initialize as an empty array
-    const [currentPage, setCurrentPage] = useState(1); // Track the current page
+    const [pokemonList, setPokemonList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [loading, setLoading] = useState(false); // Added loading state
+    const [error, setError] = useState(null); // Added error state
     const itemsPerPage = 20;
     const pageTitle = "POKEMON";
-
 
     async function getPokemon(pokemonId) {
         try {
             const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-            console.log(result);
-            return result.data; // Return the data
+            return result.data;
         } catch (e) {
             console.error(e);
             return null;
@@ -22,36 +22,35 @@ function App() {
     }
 
     useEffect(() => {
-        document.title = pageTitle;
+        setLoading(true); // Set loading to true when fetching data
+        setError(null); // Reset error state
+
         const startIdx = (currentPage - 1) * itemsPerPage + 1;
         const endIdx = currentPage * itemsPerPage;
-
-        // Define the list of Pokémon you want to fetch for the current page
         const pokemonIds = Array.from({ length: itemsPerPage }, (_, index) => startIdx + index);
 
-        // Fetch data for Pokémon and update the state
-        Promise.all(pokemonIds.map(getPokemon)) // Fixed the function name here
+        Promise.all(pokemonIds.map(getPokemon))
             .then((pokemonDataList) => {
-                // Filter out any null values (errors)
                 const filteredPokemonData = pokemonDataList.filter((data) => data !== null);
                 setPokemonList(filteredPokemonData);
+                setLoading(false); // Set loading to false when data is fetched
+            })
+            .catch((error) => {
+                setError("Error fetching Pokémon data."); // Set error message
+                setLoading(false); // Set loading to false in case of an error
             });
     }, [currentPage]);
 
-
-    // Function to handle "Next" button click
     const handleNextClick = () => {
         setCurrentPage(currentPage + 1);
     };
 
-    // Function to handle "Previous" button click
     const handlePreviousClick = () => {
         if (currentPage > 1) {
             setCurrentPage(currentPage - 1);
         }
     };
 
-    //Add capital first letter
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
@@ -61,7 +60,7 @@ function App() {
             <img
                 src={pokemonImage}
                 alt="pokemon"
-                className="pokemon-image" // Voeg een CSS-klasse toe voor stijling
+                className="pokemon-image"
             />
 
             <div className="pagination">
@@ -71,41 +70,35 @@ function App() {
                 <button type="button" onClick={handleNextClick}>Next</button>
             </div>
 
+            {loading && <p>Loading...</p>} {/* Display loading indicator */}
+            {error && <p>Error: {error}</p>} {/* Display error message */}
+
             <div className="pokemon-list">
                 {pokemonList.map((pokemon) => (
                     <div key={pokemon.id} className="pokemon-details">
-
                         <h2>{capitalizeFirstLetter(pokemon.name)}</h2>
-
                         <img
                             src={pokemon.sprites.front_default}
                             alt={`Sprite of ${pokemon.name}`}
                         />
                         <div className="pokemon-list-info">
-
-                        <p>
-                            <h3>Moves:</h3>
-                            {pokemon.moves.length}
-                        </p>
-
-                        <p>
-                            <h3>Weight:</h3>
-                            {pokemon.weight}
-                        </p>
-
-                        <h3>Abilities:</h3>
-                        <ul>
-                            {pokemon.abilities.map((ability, index) => (
-                                <li key={index}>{capitalizeFirstLetter(ability.ability.name)}</li>
-                            ))}
-                        </ul>
-
+                            <p>
+                                <h3>Moves:</h3>
+                                {pokemon.moves.length}
+                            </p>
+                            <p>
+                                <h3>Weight:</h3>
+                                {pokemon.weight}
+                            </p>
+                            <h3>Abilities:</h3>
+                            <ul>
+                                {pokemon.abilities.map((ability, index) => (
+                                    <li key={index}>{capitalizeFirstLetter(ability.ability.name)}</li>
+                                ))}
+                            </ul>
                         </div>
-
                     </div>
                 ))}
-
-
             </div>
 
             <div className="pagination">
